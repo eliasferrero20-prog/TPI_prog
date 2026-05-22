32a09e953b7739c2e0b14ed4a402509a82533837
MaximoPalasios <maximopalaciosvillamaria@gmail.com>
2026-05-21 21:10:04 -0300

    Grafico de funciones

--- TP_mat_prog_funciones.py
+++ TP_mat_prog_funciones.py
@@ -28,3 +28,24 @@ for i in range(9):
     cC = costo_C(x_actual)
     mejor_plan = determinar_mejor_plan(x_actual)
     print(x_actual, "\t  | $", cA, "\t | $", cB, "\t| $", cC, "\t|", mejor_plan)
+
+#Graficar en el intervalo dado (0 <= x <= 50)
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_grafico = np.linspace(0, 50, 500)
+y_A = costo_A(x_grafico)
+y_B = costo_B(x_grafico)
+y_C = costo_C(x_grafico)
+plt.plot(x_grafico, y_A, color="blue", linewidth=2, label="A(x) = 40x + 200")
+plt.plot(x_grafico, y_B, color="green", linewidth=2, label="B(x) = 70x + 50")
+plt.plot(x_grafico, y_C, color="red", linewidth=2, label="C(x) = -2x² + 80x + 100")
+plt.scatter(valores_x, [costo_A(i) for i in valores_x], color="black", zorder=5)
+plt.title("Análisis Comparativo de Planes de Software (UTN)", fontsize=12, fontweight="bold")
+plt.xlabel("Horas Utilizadas Mensualmente (x)")
+plt.ylabel("Costo Mensual en $ (y)")
+plt.xlim(0, 50)
+plt.ylim(0, 1000)
+plt.grid(True, linestyle="--", alpha=0.5)
+plt.legend()
+plt.show()
