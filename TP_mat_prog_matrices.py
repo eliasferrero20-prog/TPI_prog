b0ed37569d176b688bf8463ed1ec9c4937e5e0e0
ggrossidev <grossigon@gmail.com>
2026-05-27 01:45:25 -0300

    Corrección de código - Impresión Matriz

--- TP_mat_prog_matrices.py
+++ TP_mat_prog_matrices.py
@@ -8,12 +8,18 @@ C = [
     [15, 25, 20],
     [40, 10, 30]
 ]
+
+print ("")
+
 for i in range(3):
     suma_fila = 0
     for j in range(3):
         suma_fila = suma_fila + M[i][j]
     promedio_f = suma_fila / 3
     print("Tiempo promedio de ejecución - Función", i + 1, ":", promedio_f, "ms")
+
+print ("")
+
 for j in range(3):
     suma_columna = 0
     for i in range(3):
@@ -30,6 +36,8 @@ for i in range(3):
     for j in range(3):
         MT[j][i] = M[i][j]
 
-print("Matriz Transpuesta:")
-for fila in MT:
-    print(fila)
+print("\nMatriz Transpuesta:")
+for i in range(3):
+    for j in range(3):
+        print(MT[i][j], end=" ")
+    print("")
